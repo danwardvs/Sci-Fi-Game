9efa07114cc29c12b131d3b6a3fcea3a5c85e0d9
danwardvs <danwardvs@adsgames.net>
2014-10-03 18:13:57 -0400

    abort_on_error and forest background added

--- main.cpp
+++ main.cpp
@@ -2,8 +2,22 @@
 #include<alpng.h>
 
 BITMAP* buffer;
+BITMAP* planet;
+
+
+//A function to streamline error reporting in file loading
+void abort_on_error(const char *message){
+	 set_window_title("Error!");
+	 if (screen != NULL){
+	    set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
+	 }
+	 allegro_message("%s.\n %s\n", message, allegro_error);
+	 exit(-1);
+}
 
 void update(){
+
+    draw_sprite(buffer, planet,0,0);
     draw_sprite(screen,buffer,0,0);
 
 }
@@ -17,6 +31,9 @@ void update(){
 
 void setup(){
     buffer=create_bitmap(1024,768);
+
+    if (!(planet = load_bitmap("planet.png", NULL)))
+      abort_on_error("Cannot find image planet.png\nPlease check your files and try again");
 }
 
 
